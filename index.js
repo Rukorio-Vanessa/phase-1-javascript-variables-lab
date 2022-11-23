35d2e208f7648093412c7183144d25ab8f50b362
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-23 13:09:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+//expect(companyName).to.equal('Scuber');
+const companyName = 'Scuber'
+
+//expect(mostProfitableNeighborhood).to.equal('Chelsea');
+let mostProfitableNeighborhood = 'Chelsea'
+
+//expect(companyCeo).to.equal('Susan Smith');
+let companyCeo = 'Susan Smith'
